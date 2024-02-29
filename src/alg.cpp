56d460f22f2f457ffa3b25abfc78a15063273d54
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-02-29 20:09:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,14 @@
-// Copyright 2021 NNTU-CS
-
+#include <iostream>
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    while (b !=0) {
+        int c = b;
+        b = a % b;
+        a = c;
+    }
+    return a;
 }
+int main() {
+ std::cout << gcd(10,15) << std::endl;
+ return 0;
+}
+
