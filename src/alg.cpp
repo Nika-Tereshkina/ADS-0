f3a0d9757282183049166f5b659cfe605bc74904
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-02-29 21:06:56 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,15 @@
+// Copyright 2021 NNTU-CS
 #include <iostream>
 int gcd(int a, int b) {
     int c;
     while (b !=0) {
         b = a % b;
         a = c;
-    }
+}
     return a;
 }
 int main() {
- std::cout << gcd(10,15) << std::endl;
+ std::cout << gcd(10, 15) << std::endl;
  return 0;
 }
 
